1a221d84eb578e8c1f8ec930e766dd3d12cebe3b
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2022-07-17 19:26:06 +0500

    Fixed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true
   },
+
+  
+  reactStrictMode: true,
   images: {
     domains: ['cdn.dribbble.com',"lh3.googleusercontent.com"]
   }
